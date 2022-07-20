111c1b4e3a31e2d14110b49332de91e3b055bf32
Orion Edwards <orion.edwards@octopus.com>
2022-07-20 22:10:53 +1200

    Hacky experimental JSON outputFormat

--- pkg/cmd/environment/list/list.go
+++ pkg/cmd/environment/list/list.go
@@ -1,11 +1,14 @@
 package list
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/MakeNowJust/heredoc/v2"
 	"github.com/OctopusDeploy/cli/pkg/apiclient"
 	"github.com/OctopusDeploy/cli/pkg/constants"
 	"github.com/spf13/cobra"
+	"strings"
 )
 
 func NewCmdList(client apiclient.ClientFactory) *cobra.Command {
@@ -28,8 +31,27 @@ func NewCmdList(client apiclient.ClientFactory) *cobra.Command {
 				return err
 			}
 
-			for _, e := range environments {
-				fmt.Printf("%s\t%s\t%s\n", e.GetID(), e.Name, e.Description)
+			outputFormat, _ := cmd.Flags().GetString("outputFormat")
+			switch strings.ToLower(outputFormat) {
+			// TODO make a proper thing that encodes JSON for us so we don't have to write this boilerplate a zillion times
+			case "json":
+				type IdName struct {
+					Id   string `json:"Id"`
+					Name string `json:"Name"`
+				}
+				outputJson := []IdName{}
+				for _, e := range environments {
+					outputJson = append(outputJson, IdName{Id: e.GetID(), Name: e.Name})
+				}
+
+				data, _ := json.MarshalIndent(outputJson, "", "  ")
+				fmt.Println(string(data))
+			case "":
+				for _, e := range environments {
+					fmt.Printf("%s\t%s\t%s\n", e.GetID(), e.Name, e.Description)
+				}
+			default:
+				return errors.New(fmt.Sprintf("Unsupported outputFormat %s. Valid values are 'json' or an empty string", outputFormat))
 			}
 
 			return nil
