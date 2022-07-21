550301c8aa472fd183802a8ac200ce5538ac8d20
John Bristowe <john.bristowe@octopus.com>
2022-07-21 15:23:08 +1000

    feat: added interactive delete

--- pkg/cmd/space/delete/delete.go
+++ pkg/cmd/space/delete/delete.go
@@ -1,23 +1,22 @@
 package delete
 
 import (
-	"errors"
 	"fmt"
+	"io"
 	"strings"
-
-	"github.com/OctopusDeploy/cli/pkg/question"
-	"github.com/OctopusDeploy/cli/pkg/usage"
 
 	"github.com/MakeNowJust/heredoc/v2"
 	"github.com/OctopusDeploy/cli/pkg/apiclient"
 	"github.com/OctopusDeploy/cli/pkg/constants"
+	"github.com/OctopusDeploy/cli/pkg/output"
+	"github.com/OctopusDeploy/cli/pkg/question"
 	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/services"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/spaces"
 	"github.com/spf13/cobra"
 )
 
 func NewCmdDelete(f apiclient.ClientFactory) *cobra.Command {
 	cmd := &cobra.Command{
-		Args:    usage.ExactArgs(1),
 		Use:     "delete {<name> | <id>}",
 		Short:   "Delete a space in an instance of Octopus Deploy",
 		Long:    "Delete a space in an instance of Octopus Deploy.",
@@ -28,9 +27,11 @@ func NewCmdDelete(f apiclient.ClientFactory) *cobra.Command {
 		`), constants.ExecutableName, constants.ExecutableName),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if len(args) == 0 {
-				return errors.New("please specify the name or ID of the space to delete")
+				deleteRun(f, cmd.OutOrStdout())
+				return nil
 			}
-			itemIDorName := strings.TrimSpace(args[0])
+
+			itemIDOrName := strings.TrimSpace(args[0])
 
 			alreadyConfirmed, err := cmd.Flags().GetBool("confirm")
 			if err != nil {
@@ -42,39 +43,90 @@ func NewCmdDelete(f apiclient.ClientFactory) *cobra.Command {
 				return err
 			}
 			// TODO go to the server and lookup the space id/name and how many projects it has
-			space, err := client.Spaces.GetByIDOrName(itemIDorName)
+			spaceToDelete, err := client.Spaces.GetByIDOrName(itemIDOrName)
 			if err != nil { // could be services.itemNotFound if they typed it wrong.
 				if err == services.ErrItemNotFound {
-					return fmt.Errorf("cannot find a space with name or ID of '%s'", itemIDorName)
+					return fmt.Errorf("cannot find a space with name or ID of '%s'", itemIDOrName)
 				}
 				return err
 			}
 
 			if !alreadyConfirmed { // TODO NO_PROMPT env var or whatever we do there
-				err = question.AskForDeleteConfirmation("space", space.Name, space.GetID())
+				err = question.AskForDeleteConfirmation("space", spaceToDelete.Name, spaceToDelete.GetID())
 				if err != nil {
 					return err
 				}
 			}
 
 			// we need to stop the task queue on a space before we can delete it
-			space.TaskQueueStopped = true
-			space, err = client.Spaces.Update(space)
-			if err != nil { // e.g can't stop the task queue
+			if !spaceToDelete.TaskQueueStopped {
+				spaceToDelete.TaskQueueStopped = true
+				if _, err := client.Spaces.Update(spaceToDelete); err != nil {
+					return err
+				}
+			}
+
+			if err := client.Spaces.DeleteByID(spaceToDelete.ID); err != nil {
 				return err
 			}
 
-			err = client.Spaces.DeleteByID(space.GetID())
-			if err != nil { // e.g can't stop the task queue
-				return err
-			}
-
-			cmd.Printf("Deleted Space %s (%s).\n", space.Name, space.GetID())
-			return err
+			cmd.Printf("%s The space, \"%s\" %s was deleted successfully.\n", output.Red("✔"), spaceToDelete.Name, output.Dimf("(%s)", spaceToDelete.ID))
+			return nil
 		},
 	}
 	// TODO confirm might want to be a global flag?
 	cmd.Flags().BoolP("confirm", "y", false, "Don't ask for confirmation before deleting the space.")
 
 	return cmd
+}
+
+func deleteRun(f apiclient.ClientFactory, w io.Writer) error {
+	client, err := f.Get(false)
+	if err != nil {
+		return err
+	}
+
+	existingSpaces, err := client.Spaces.GetAll()
+	if err != nil {
+		return err
+	}
+
+	spaceToDelete, err := selectSpace(existingSpaces, "Select the space you wish to delete:")
+	if err != nil {
+		return err
+	}
+
+	if err := question.AskForDeleteConfirmation("space", spaceToDelete.Name, spaceToDelete.ID); err != nil {
+		return err
+	}
+
+	if !spaceToDelete.TaskQueueStopped {
+		spaceToDelete.TaskQueueStopped = true
+		if _, err := client.Spaces.Update(spaceToDelete); err != nil {
+			return err
+		}
+	}
+
+	if err := client.Spaces.DeleteByID(spaceToDelete.ID); err != nil {
+		return err
+	}
+
+	fmt.Printf("%s The space, \"%s\" %s was deleted successfully.\n", output.Red("✔"), spaceToDelete.Name, output.Dimf("(%s)", spaceToDelete.ID))
+	return nil
+}
+
+func selectSpace(existingSpaces []*spaces.Space, message string) (*spaces.Space, error) {
+	var selectedSpace *spaces.Space
+	if err := question.Select(message, existingSpaces, func(space *spaces.Space) string {
+		for _, existingSpace := range existingSpaces {
+			if space.ID == existingSpace.ID {
+				return fmt.Sprintf("%s %s", space.Name, output.Dimf("(%s)", space.ID))
+			}
+		}
+		return ""
+	}, &selectedSpace); err != nil {
+		return nil, err
+	}
+
+	return selectedSpace, nil
 }
