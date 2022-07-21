3f083f3f044327a830439b2b724303e6b4f218cb
John Bristowe <john.bristowe@octopus.com>
2022-07-22 06:29:36 +1000

    feat: added validator

--- pkg/question/ask.go
+++ pkg/question/ask.go
@@ -1,7 +1,31 @@
 package question
 
-import "github.com/AlecAivazis/survey/v2"
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/AlecAivazis/survey/v2"
+)
 
 var AskOne = survey.AskOne
-
 var Ask = survey.Ask
+
+// NotEquals requires that the string does not equal any of the specified values
+func NotEquals(stringsToCheck []string, errorMessage string) survey.Validator {
+	// return a validator to perform the check
+	return func(val interface{}) error {
+		if str, ok := val.(string); ok {
+			for _, v := range stringsToCheck {
+				if str == v {
+					return fmt.Errorf("%s", errorMessage)
+				}
+			}
+		} else {
+			// otherwise we cannot convert the value into a string and cannot perform check
+			return fmt.Errorf("cannot check value on response of type %v", reflect.TypeOf(val).Name())
+		}
+
+		// the input is fine
+		return nil
+	}
+}
