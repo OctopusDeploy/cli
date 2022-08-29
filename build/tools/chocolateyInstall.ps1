d4ad0e28ebbd873fb1d0760e8d7f74f7bfb97dca
hnrkndrssn <henrik@henrikandersson.org>
2022-08-29 12:45:48 +1000

    chore: add chocolatey package assets

---
+++ build/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$logMsi = Join-Path -Path $env:TEMP -ChildPath ("{0}-{1}-MsiInstall.log" -f $env:ChocolateyPackageName, $env:chocolateyPackageVersion)
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'MSI'
+    silentArgs     = "/qn /norestart /l*v `"$logMsi`""
+    file64         = Join-Path -Path $toolsDir -ChildPath "octopus_$($env:ChocolateyPackageVersion)_Windows_x86_64.msi"
+}
+
+Install-ChocolateyInstallPackage @packageArgs
