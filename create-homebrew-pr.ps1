70e1e7879fc06872bde48c9d41162cce23098e9f
Orion Edwards <orion.edwards@octopus.com>
2022-09-13 21:22:50 +1200

    does this even work?

--- create-homebrew-pr.ps1
+++ create-homebrew-pr.ps1
@@ -3,6 +3,7 @@
 origin=borland/homebrew-core # should be Homewbrew/homebrew-core for production
 packageVersion="0.2.0"
 packageUrl="https://github.com/OctopusDeploy/cli/releases/download/v${packageVersion}/octopus_${packageVersion}_Darwin_arm64.tar.gz"
+formulaFile="octopus-cli.rb"
 
 Invoke-WebRequest $packageUrl -outfile pkg.tgz
 sha256=(Get-FileHash pkg.tgz -a sha256).Hash.ToLowerInvariant()
@@ -10,6 +11,20 @@ rm pkg.tgz
 
 # git clone --depth 1 $origin our-homebrew-core
 # cd our-homebrew-core/Formula
+
+# git checkout -b bump-octopus-cli-$packageVersion
+
+((Get-Content $formulaFile)
+     -replace "^version \".*\"$", "version \"$packageVersion\""
+     -replace "^url \".*\"$", "url \"$packageUrl\""
+     -replace "^sha256 \".*\"$", "sha256 \"$sha256\"") | Set-Content $formulaFile
+
+# git commit -a -m "octopus-cli $packageVersion"
+
+# gh pr create --base $origin --title "octopus-cli $packageVersion"
+
+
+
 
 
 
