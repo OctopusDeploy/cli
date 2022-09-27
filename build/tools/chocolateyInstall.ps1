f64ae3ed8f2f334d461501038f0d884f97362d35
hnrkndrssn <henrik@henrikandersson.org>
2022-09-28 09:03:07 +1000

    fix: Update name of msi and add missing log argument to MSI call

--- build/tools/chocolateyInstall.ps1
+++ build/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $logMsi = Join-Path -Path $env:TEMP -ChildPath ("{0}-{1}-MsiInstall.log" -f $env
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'MSI'
-    silentArgs     = "/qn /norestart `"$logMsi`""
-    file64         = Join-Path -Path $toolsDir -ChildPath "octopus_$($env:ChocolateyPackageVersion)_windows_amd64.msi"
+    silentArgs     = "/qn /norestart /log `"$logMsi`""
+    file64         = Join-Path -Path $toolsDir -ChildPath "octopus_$($env:ChocolateyPackageVersion)_Windows_x86_64.msi"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
