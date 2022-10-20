1dd4a5b43e5e89e43b5da0c3e67ce6bb7a1a73cc
domenicsim1 <87625140+domenicsim1@users.noreply.github.com>
2022-10-21 10:17:19 +1100

    chore: fix typo
    Co-authored-by: John Bristowe <john.bristowe@octopus.com>

--- pkg/cmd/task/wait/wait.go
+++ pkg/cmd/task/wait/wait.go
@@ -43,7 +43,7 @@ func NewCmdWait(f factory.Factory) *cobra.Command {
 		Short: "Wait for task(s) to finish",
 		Long:  "Wait for a provided list of task(s) to finish",
 		Example: heredoc.Docf(`
-			$ %s octopus task wait
+			$ %s task wait
 		`, constants.ExecutableName),
 		RunE: func(c *cobra.Command, args []string) error {
 			taskIDs := make([]string, len(args))
