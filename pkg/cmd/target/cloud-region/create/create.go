0474d6b6f7ede6f2742e2c333387684bdd034c95
Ben Pearce <ben.pearce@octopus.com>
2022-11-09 23:05:19 +1000

    initial working create cloud region target

---
+++ pkg/cmd/target/cloud-region/create/create.go
@@ -0,0 +1,156 @@
+package create
+
+import (
+	"fmt"
+	"github.com/MakeNowJust/heredoc/v2"
+	"github.com/OctopusDeploy/cli/pkg/cmd"
+	"github.com/OctopusDeploy/cli/pkg/cmd/target/shared"
+	"github.com/OctopusDeploy/cli/pkg/constants"
+	"github.com/OctopusDeploy/cli/pkg/executionscommon"
+	"github.com/OctopusDeploy/cli/pkg/factory"
+	"github.com/OctopusDeploy/cli/pkg/question"
+	"github.com/OctopusDeploy/cli/pkg/util"
+	"github.com/OctopusDeploy/cli/pkg/util/flag"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/environments"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/machines"
+	"github.com/spf13/cobra"
+)
+
+const (
+	FlagName = "name"
+)
+
+type CreateFlags struct {
+	Name *flag.Flag[string]
+	*shared.CreateTargetEnvironmentFlags
+	*shared.CreateTargetRoleFlags
+	*shared.CreateTargetWorkerPoolFlags
+	*shared.CreateTargetTenantFlags
+}
+
+type CreateOptions struct {
+	*CreateFlags
+	*shared.CreateTargetEnvironmentOptions
+	*shared.CreateTargetRoleOptions
+	*shared.CreateTargetWorkerPoolOptions
+	*shared.CreateTargetTenantOptions
+	*cmd.Dependencies
+}
+
+func NewCreateFlags() *CreateFlags {
+	return &CreateFlags{
+		Name:                         flag.New[string](FlagName, false),
+		CreateTargetWorkerPoolFlags:  shared.NewCreateTargetWorkerPoolFlags(),
+		CreateTargetEnvironmentFlags: shared.NewCreateTargetEnvironmentFlags(),
+		CreateTargetRoleFlags:        shared.NewCreateTargetRoleFlags(),
+		CreateTargetTenantFlags:      shared.NewCreateTargetTenantFlags(),
+	}
+}
+
+func NewCreateOptions(createFlags *CreateFlags, dependencies *cmd.Dependencies) *CreateOptions {
+	return &CreateOptions{
+		CreateFlags:                    createFlags,
+		Dependencies:                   dependencies,
+		CreateTargetWorkerPoolOptions:  shared.NewCreateTargetWorkerPoolOptions(dependencies),
+		CreateTargetEnvironmentOptions: shared.NewCreateTargetEnvironmentOptions(dependencies),
+		CreateTargetRoleOptions:        shared.NewCreateTargetRoleOptions(dependencies),
+		CreateTargetTenantOptions:      shared.NewCreateTargetTenantOptions(dependencies),
+	}
+}
+
+func NewCmdCreate(f factory.Factory) *cobra.Command {
+	createFlags := NewCreateFlags()
+
+	cmd := &cobra.Command{
+		Use:   "create",
+		Short: "Create a cloud region deployment target",
+		Long:  "Create a cloud region deployment target in Octopus Deploy",
+		Example: fmt.Sprintf(heredoc.Doc(`
+			$ %s deployment-target cloud-region create
+		`), constants.ExecutableName),
+		RunE: func(c *cobra.Command, _ []string) error {
+			opts := NewCreateOptions(createFlags, cmd.NewDependencies(f, c))
+
+			return createRun(opts)
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringVarP(&createFlags.Name.Value, createFlags.Name.Name, "n", "", "A short, memorable, unique name for this Cloud Region.")
+	shared.RegisterCreateTargetEnvironmentFlags(cmd, createFlags.CreateTargetEnvironmentFlags)
+	shared.RegisterCreateTargetRoleFlags(cmd, createFlags.CreateTargetRoleFlags)
+	shared.RegisterCreateTargetWorkerPoolFlags(cmd, createFlags.CreateTargetWorkerPoolFlags)
+	shared.RegisterCreateTargetTenantFlags(cmd, createFlags.CreateTargetTenantFlags)
+
+	return cmd
+}
+
+func createRun(opts *CreateOptions) error {
+	if !opts.NoPrompt {
+		if err := PromptMissing(opts); err != nil {
+			return err
+		}
+	}
+
+	envs, err := executionscommon.FindEnvironments(opts.Client, opts.Environments.Value)
+	if err != nil {
+		return err
+	}
+	environmentIds := util.SliceTransform(envs, func(e *environments.Environment) string { return e.ID })
+
+	endpoint := machines.NewCloudRegionEndpoint()
+	if opts.WorkerPool.Value != "" {
+		workerPoolId, err := shared.FindWorkerPoolId(opts.GetAllWorkerPoolsCallback, opts.WorkerPool.Value)
+		if err != nil {
+			return err
+		}
+		endpoint.DefaultWorkerPoolID = workerPoolId
+	}
+
+	target := machines.NewDeploymentTarget(opts.Name.Value, endpoint, environmentIds, shared.DistinctRoles(opts.Roles.Value))
+	err = shared.ConfigureTenant(target, opts.CreateTargetTenantFlags, opts.CreateTargetTenantOptions)
+	if err != nil {
+		return err
+	}
+
+	_, err = opts.Client.Machines.Add(target)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(opts.Out, "Successfully created cloud region '%s'.\n", target.Name)
+	if !opts.NoPrompt {
+		autoCmd := flag.GenerateAutomationCmd(opts.CmdPath, opts.Name, opts.WorkerPool, opts.Environments, opts.Roles, opts.TenantedDeploymentMode, opts.Tenants, opts.TenantTags)
+		fmt.Fprintf(opts.Out, "\nAutomation Command: %s\n", autoCmd)
+	}
+
+	return nil
+}
+
+func PromptMissing(opts *CreateOptions) error {
+	err := question.AskName(opts.Ask, "", "Cloud Region", &opts.Name.Value)
+	if err != nil {
+		return err
+	}
+
+	err = shared.PromptForEnvironments(opts.CreateTargetEnvironmentOptions, opts.CreateTargetEnvironmentFlags)
+	if err != nil {
+		return err
+	}
+
+	err = shared.PromptForRoles(opts.CreateTargetRoleOptions, opts.CreateTargetRoleFlags)
+	if err != nil {
+		return err
+	}
+
+	err = shared.PromptForWorkerPool(opts.CreateTargetWorkerPoolOptions, opts.CreateTargetWorkerPoolFlags)
+	if err != nil {
+		return err
+	}
+
+	err = shared.PromptForTenant(opts.CreateTargetTenantOptions, opts.CreateTargetTenantFlags)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ pkg/cmd/target/shared/workerpool.go
@@ -0,0 +1,99 @@
+package shared
+
+import (
+	"fmt"
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/OctopusDeploy/cli/pkg/cmd"
+	"github.com/OctopusDeploy/cli/pkg/question/selectors"
+	"github.com/OctopusDeploy/cli/pkg/util"
+	"github.com/OctopusDeploy/cli/pkg/util/flag"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/client"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/workerpools"
+	"github.com/spf13/cobra"
+	"strings"
+)
+
+const FlagWorkerPool = "worker-pool"
+
+type GetAllWorkerPoolsCallback func() ([]*workerpools.IWorkerPool, error)
+
+type CreateTargetWorkerPoolFlags struct {
+	WorkerPool *flag.Flag[string]
+}
+
+type CreateTargetWorkerPoolOptions struct {
+	*cmd.Dependencies
+	GetAllWorkerPoolsCallback
+}
+
+func NewCreateTargetWorkerPoolFlags() *CreateTargetWorkerPoolFlags {
+	return &CreateTargetWorkerPoolFlags{
+		WorkerPool: flag.New[string](FlagWorkerPool, false),
+	}
+}
+
+func NewCreateTargetWorkerPoolOptions(dependencies *cmd.Dependencies) *CreateTargetWorkerPoolOptions {
+	return &CreateTargetWorkerPoolOptions{
+		Dependencies: dependencies,
+		GetAllWorkerPoolsCallback: func() ([]*workerpools.IWorkerPool, error) {
+			return getAllWorkerPools(*dependencies.Client)
+		},
+	}
+}
+
+func RegisterCreateTargetWorkerPoolFlags(cmd *cobra.Command, flags *CreateTargetWorkerPoolFlags) {
+	cmd.Flags().StringVar(&flags.WorkerPool.Value, flags.WorkerPool.Name, "", "")
+}
+
+func PromptForWorkerPool(opts *CreateTargetWorkerPoolOptions, flags *CreateTargetWorkerPoolFlags) error {
+	if flags.WorkerPool.Value == "" {
+		useDefaultPool := true
+		opts.Ask(&survey.Confirm{
+			Message: "Will this worker use the default worker pool?",
+		}, &useDefaultPool)
+		if !useDefaultPool {
+			selectedPool, err := selectors.Select[workerpools.IWorkerPool](
+				opts.Ask,
+				"Select the worker pool to use",
+				opts.GetAllWorkerPoolsCallback,
+				func(p *workerpools.IWorkerPool) string {
+					return (*p).GetName()
+				})
+			if err != nil {
+				return err
+			}
+			flags.WorkerPool.Value = (*selectedPool).GetName()
+		}
+	}
+
+	return nil
+}
+
+func FindWorkerPoolId(getAllWorkerPools GetAllWorkerPoolsCallback, nameOrId string) (string, error) {
+	pools, err := getAllWorkerPools()
+	if err != nil {
+		return "", err
+	}
+
+	for _, p := range pools {
+		pool := (*p)
+		if strings.EqualFold(nameOrId, pool.GetID()) || strings.EqualFold(nameOrId, pool.GetName()) {
+			return pool.GetID(), nil
+		}
+	}
+
+	return "", fmt.Errorf("Cannot find worker pool '%s'", nameOrId)
+}
+
+func getAllWorkerPools(client client.Client) ([]*workerpools.IWorkerPool, error) {
+	res, err := client.WorkerPools.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var pools []*workerpools.IWorkerPool
+	pools = util.SliceTransform(res, func(p workerpools.IWorkerPool) *workerpools.IWorkerPool {
+		return &p
+	})
+	return pools, nil
+}
