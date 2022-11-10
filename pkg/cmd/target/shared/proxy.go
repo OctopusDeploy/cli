eb4dce45360a69063de1f5d1d0fd9bd136887202
Ben Pearce <ben.pearce@octopus.com>
2022-11-10 12:28:47 +1000

    fix up default

--- pkg/cmd/target/shared/proxy.go
+++ pkg/cmd/target/shared/proxy.go
@@ -17,6 +17,7 @@ func PromptForProxy(opts *CreateTargetProxyOptions, flags *CreateTargetProxyFlag
 		directConnection := true
 		opts.Ask(&survey.Confirm{
 			Message: "Should the connection to the tentacle be direct?",
+			Default: true,
 		}, &directConnection)
 		if !directConnection {
 			selectedOption, err := selectors.Select(opts.Ask, "Select the proxy to use", opts.GetAllProxiesCallback, func(p *proxies.Proxy) string { return p.GetName() })
