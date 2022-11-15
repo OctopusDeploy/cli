5c8ee3db3e06f0568b904c6b6f48d077ad0856ce
Ben Pearce <ben.pearce@octopus.com>
2022-11-15 21:27:24 +1000

    feat: deployment-target ssh list

--- pkg/cmd/target/ssh/ssh.go
+++ pkg/cmd/target/ssh/ssh.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	cmdCreate "github.com/OctopusDeploy/cli/pkg/cmd/target/ssh/create"
+	cmdList "github.com/OctopusDeploy/cli/pkg/cmd/target/ssh/list"
 	"github.com/OctopusDeploy/cli/pkg/constants"
 	"github.com/OctopusDeploy/cli/pkg/factory"
 	"github.com/spf13/cobra"
@@ -18,5 +19,6 @@ func NewCmdSsh(f factory.Factory) *cobra.Command {
 	}
 
 	cmd.AddCommand(cmdCreate.NewCmdCreate(f))
+	cmd.AddCommand(cmdList.NewCmdList(f))
 	return cmd
 }
