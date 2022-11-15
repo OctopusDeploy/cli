eb559763351e2afef412ff155c441901e070bbe1
Ben Pearce <ben.pearce@octopus.com>
2022-11-15 20:20:08 +1000

    feat: deployment-target listening-tentacle list

--- pkg/cmd/target/listening-tentacle/listening-tentacle.go
+++ pkg/cmd/target/listening-tentacle/listening-tentacle.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	cmdCreate "github.com/OctopusDeploy/cli/pkg/cmd/target/listening-tentacle/create"
+	cmdList "github.com/OctopusDeploy/cli/pkg/cmd/target/listening-tentacle/list"
 	"github.com/OctopusDeploy/cli/pkg/constants"
 	"github.com/OctopusDeploy/cli/pkg/factory"
 	"github.com/spf13/cobra"
@@ -17,7 +18,7 @@ func NewCmdListeningTentacle(f factory.Factory) *cobra.Command {
 		Example: fmt.Sprintf("$ %s deployment-target listening-tenatacle list", constants.ExecutableName),
 	}
 
-	//cmd.AddCommand(cmdList.NewCmdList(f))
+	cmd.AddCommand(cmdList.NewCmdList(f))
 	cmd.AddCommand(cmdCreate.NewCmdCreate(f))
 	return cmd
 }
