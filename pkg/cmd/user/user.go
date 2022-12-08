74fdc7e0a811dcc6444d2fd48df3cbb661e87503
Orion Edwards <orion.edwards@octopus.com>
2022-12-09 10:49:45 +1300

    feat: basic `user list` and `user delete`

---
+++ pkg/cmd/user/user.go
@@ -0,0 +1,31 @@
+package user
+
+import (
+	"github.com/MakeNowJust/heredoc/v2"
+	cmdDelete "github.com/OctopusDeploy/cli/pkg/cmd/user/delete"
+	cmdList "github.com/OctopusDeploy/cli/pkg/cmd/user/list"
+	"github.com/OctopusDeploy/cli/pkg/constants"
+	"github.com/OctopusDeploy/cli/pkg/constants/annotations"
+	"github.com/OctopusDeploy/cli/pkg/factory"
+	"github.com/spf13/cobra"
+)
+
+func NewCmdUser(f factory.Factory) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "user <command>",
+		Short: "Manage users",
+		Long:  "Manage user in Octopus Deploy",
+		Example: heredoc.Docf(`
+			$ %[1]s user list
+			$ %[1]s user ls
+		`, constants.ExecutableName),
+		Annotations: map[string]string{
+			annotations.IsCore: "true",
+		},
+	}
+
+	cmd.AddCommand(cmdList.NewCmdList(f))
+	cmd.AddCommand(cmdDelete.NewCmdDelete(f))
+
+	return cmd
+}
