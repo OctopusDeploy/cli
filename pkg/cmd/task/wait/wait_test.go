40ac7150ea3a4930fe4e7f86a1f620f0d83ab431
Huy Nguyen <huy.nguyen@octopus.com>
2025-02-13 16:59:58 +1000

    fix test fail

--- pkg/cmd/task/wait/wait_test.go
+++ pkg/cmd/task/wait/wait_test.go
@@ -39,7 +39,7 @@ func TestWait(t *testing.T) {
 	taskList[0].Description = "Deploy Bar 1 release 0.0.2 to Foo"
 	taskList[0].State = "Executing"
 
-	taskList[1].ID = defaultTaskIDs[0]
+	taskList[1].ID = defaultTaskIDs[1]
 	taskList[1].IsCompleted = &boolTrue
 	taskList[1].FinishedSuccessfully = &boolTrue
 	taskList[1].Description = "Deploy Bar 2 release 0.0.2 to Foo"
@@ -70,9 +70,9 @@ func TestWait(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 2, timesCalled)
 	expectedOutput := heredoc.Doc(`
-  Deploy Bar 1 release 0.0.2 to Foo: Executing
-  Deploy Bar 2 release 0.0.2 to Foo: Success
-  Deploy Bar 1 release 0.0.2 to Foo: Success
+  TaskID1: Deploy Bar 1 release 0.0.2 to Foo: Executing
+  TaskID2: Deploy Bar 2 release 0.0.2 to Foo: Success
+  TaskID1: Deploy Bar 1 release 0.0.2 to Foo: Success
   `)
 	assert.Equal(t, expectedOutput, out.String())
 }
@@ -101,9 +101,9 @@ func TestWait_FailedTask(t *testing.T) {
 		return taskList, nil
 	}
 	err := taskWaitCreate.WaitRun(&out, defaultTaskIDs, getServerTaskCallback, nil, taskWaitCreate.DefaultTimeout, false)
-	assert.EqualError(t, err, "One or more deployment tasks failed.")
+	assert.EqualError(t, err, "One or more deployment tasks failed: TaskID1")
 	expectedOutput := heredoc.Doc(`
-  Deploy Bar 1 release 0.0.2 to Foo: Failed
+  TaskID1: Deploy Bar 1 release 0.0.2 to Foo: Failed
   `)
 	assert.Equal(t, expectedOutput, out.String())
 }
@@ -144,11 +144,11 @@ func TestWait_FailedPendingTask(t *testing.T) {
 		return nil, fmt.Errorf("getServerTaskCallback was called more then the expected amount of times")
 	}
 	err := taskWaitCreate.WaitRun(&out, defaultTaskIDs, getServerTaskCallback, nil, taskWaitCreate.DefaultTimeout, false)
-	assert.EqualError(t, err, "One or more deployment tasks failed.")
+	assert.EqualError(t, err, "One or more deployment tasks failed: TaskID1")
 	assert.Equal(t, 2, timesCalled)
 	expectedOutput := heredoc.Doc(`
-  Deploy Bar 1 release 0.0.2 to Foo: Executing
-  Deploy Bar 1 release 0.0.2 to Foo: Failed
+  TaskID1: Deploy Bar 1 release 0.0.2 to Foo: Executing
+  TaskID1: Deploy Bar 1 release 0.0.2 to Foo: Failed
   `)
 	assert.Equal(t, expectedOutput, out.String())
 }
