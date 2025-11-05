0fc0cddda3f30343a1f6f72d4bf3429d7e729a48
Caitlyn Stocker <43163630+caitlynstocker@users.noreply.github.com>
2025-11-05 15:35:27 +1000

    feat: Add ephemeral environment create command (#559)
    * Add command to create an ephemeral environment
    * Add tests of the new command

---
+++ pkg/cmd/ephemeralenvironment/ephemeralenvironment.go
@@ -0,0 +1,27 @@
+package ephemeralenvironment
+
+import (
+	"github.com/MakeNowJust/heredoc/v2"
+	cmdCreate "github.com/OctopusDeploy/cli/pkg/cmd/ephemeralenvironment/create"
+	"github.com/OctopusDeploy/cli/pkg/constants"
+	"github.com/OctopusDeploy/cli/pkg/constants/annotations"
+	"github.com/OctopusDeploy/cli/pkg/factory"
+	"github.com/spf13/cobra"
+)
+
+func NewCmdEphemeralEnvironment(f factory.Factory) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "ephemeral-environment <command>",
+		Short: "Manage ephemeral environments",
+		Long:  "Manage ephemeral environments in Octopus Deploy",
+		Example: heredoc.Docf(`
+			$ %[1]s ephemeral-environment create --name "MyEphemeralEnvironment" --project "MyProject"
+		`, constants.ExecutableName),
+		Annotations: map[string]string{
+			annotations.IsInfrastructure: "true",
+		},
+	}
+
+	cmd.AddCommand(cmdCreate.NewCmdCreate(f))
+	return cmd
+}
