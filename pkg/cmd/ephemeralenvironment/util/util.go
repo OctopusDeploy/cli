fd454a3414fff078166450312526b85e65efba94
Lourens de Jager <165963988+lourens-octopus@users.noreply.github.com>
2025-11-12 10:40:18 +1300

    chore: Refactor output method

--- pkg/cmd/ephemeralenvironment/util/util.go
+++ pkg/cmd/ephemeralenvironment/util/util.go
@@ -1,11 +1,15 @@
 package util
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 
+	"github.com/OctopusDeploy/cli/pkg/constants"
+	"github.com/OctopusDeploy/cli/pkg/output"
 	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/client"
 	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/environments/v2/ephemeralenvironments"
+	"github.com/spf13/cobra"
 )
 
 func GetByName(client *client.Client, name string, spaceID string) (*ephemeralenvironments.EphemeralEnvironment, error) {
@@ -33,5 +37,51 @@ func GetByName(client *client.Client, name string, spaceID string) (*ephemeralen
 		}
 
 		return nil, fmt.Errorf("could not find an exact match of an ephemeral environment with the name '%s'. Please specify a more specific name", name)
+	}
+}
+
+func OutPutDeprovisionResult(environmentName string, command *cobra.Command, deprovisioningRuns []ephemeralenvironments.DeprovisioningRunbookRun) {
+	command.Println("Deprovisioning environment: " + environmentName)
+
+	outputFormat, err := command.Flags().GetString(constants.FlagOutputFormat)
+	if err != nil {
+		outputFormat = constants.OutputFormatTable
+	}
+
+	if deprovisioningRuns == nil {
+		deprovisioningRuns = []ephemeralenvironments.DeprovisioningRunbookRun{}
+	}
+
+	switch outputFormat {
+	case constants.OutputFormatBasic:
+		if len(deprovisioningRuns) == 0 {
+			command.Println("Environment deprovisioned without running a runbook.")
+		} else {
+			for _, run := range deprovisioningRuns {
+				command.Printf("Runbook Run ID: %s \nServer Task ID %s \n", run.RunbookRunID, run.TaskId)
+			}
+		}
+	case constants.OutputFormatJson:
+		data, err := json.Marshal(deprovisioningRuns)
+		if err != nil {
+			command.PrintErrln(err)
+		} else {
+			_, _ = command.OutOrStdout().Write(data)
+			command.Println()
+		}
+	default:
+		if len(deprovisioningRuns) == 0 {
+			command.Println("Environment deprovisioned without running a runbook.")
+		} else {
+			t := output.NewTable(command.OutOrStdout())
+			t.AddRow(output.Bold("Runbook Run ID"), output.Bold("Server Task ID"))
+			for _, run := range deprovisioningRuns {
+				t.AddRow(run.RunbookRunID, run.TaskId)
+			}
+			err := t.Print()
+			if err != nil {
+				command.PrintErrln(err)
+			}
+		}
 	}
 }
